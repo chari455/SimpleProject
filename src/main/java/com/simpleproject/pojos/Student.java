d30cc85d19368b03ed746a4569fb7dd261feca27
usar <usar@DESKTOP-BMP6C86>
2019-11-24 12:25:21 +0530

    conflict

--- src/main/java/com/simpleproject/pojos/Student.java
+++ src/main/java/com/simpleproject/pojos/Student.java
@@ -5,6 +5,11 @@ public class Student {
 public static void main(String[] args) {
 	System.out.println("TODO..");
 	System.out.println("working in progress..");
-	
+	System.out.println("1");
+	System.out.println("1");
+	System.out.println("1");
+	System.out.println("1");
+	System.out.println("1");
+	System.out.println("1");
 }
 }
