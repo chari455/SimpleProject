17005b319487f2d1417d6bda4ebdacaa68abc418
usar <usar@DESKTOP-BMP6C86>
2019-11-24 13:02:26 +0530

    jgvgvgh

--- src/main/java/com/simpleproject/pojos/Demo.java
+++ src/main/java/com/simpleproject/pojos/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
 public static void main(String[] args) {
 	
 	
-<<<<<<< HEAD
+
 	System.out.println("Demo Class");
 	System.out.println("Demo Class");
 	System.out.println("Demo Class");
@@ -16,9 +16,9 @@ public static void main(String[] args) {
 	System.out.println("Demo Class");
 	System.out.println("Demo Class");
 	System.out.println("Demo Class");
-=======
+
 	System.out.println("Demo");
->>>>>>> branch 'master' of https://github.com/chari455/SimpleProject.git
+ branch 'master' of https://github.com/chari455/SimpleProject.git
 }
 
 
@@ -28,10 +28,9 @@ public static void main(String[] args) {
 
 
 
-<<<<<<< HEAD
 
 	
 }
-=======
+
 	
->>>>>>> branch 'master' of https://github.com/chari455/SimpleProject.git
+ branch 'master' of https://github.com/chari455/SimpleProject.git
