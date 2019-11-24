f3dba0594212ef083b7ab8a5172cf4469b6021e4
SRINIVAS <SRINIVAS@DESKTOP-63FOQ9V>
2019-11-24 13:17:33 +0530

    dao..

--- src/main/java/com/simpleproject/dao/StudentDao.java
+++ src/main/java/com/simpleproject/dao/StudentDao.java
@@ -5,5 +5,6 @@ public class StudentDao {
 	public static void main(String[] args) {
 		System.out.println("Todo...");
 		System.out.println("..");
+		System.out.println("working in progress..");
 	}
 }
