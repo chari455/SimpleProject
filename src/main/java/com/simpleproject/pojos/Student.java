d3a40e50ec05c2e425e2d6dcdb8facbf1900ae45
usar <usar@DESKTOP-BMP6C86>
2019-11-24 12:32:08 +0530

    pojo

--- src/main/java/com/simpleproject/pojos/Student.java
+++ src/main/java/com/simpleproject/pojos/Student.java
@@ -11,5 +11,7 @@ public static void main(String[] args) {
 	System.out.println("1");
 	System.out.println("1");
 	System.out.println("1");
-}
+	System.out.println("1");
+	System.out.println("1");
+	}
 }
